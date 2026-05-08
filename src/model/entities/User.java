669b1470418130074c7d6e2ca06795c24a6c1988
Marcos Ferreira <markosgamedev@gmail.com>
2026-05-08 13:09:41 -0300

    User entity class update

--- src/model/entities/User.java
+++ src/model/entities/User.java
@@ -1,9 +1,12 @@
 package model.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class User {
+public class User implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
 	private String name;
 	private String email;
