39ec125368b122019a92f3bee5b2f6d2138a6838
Marcos Ferreira <markosgamedev@gmail.com>
2026-05-08 13:06:14 -0300

    User entity class

---
+++ src/model/entities/User.java
@@ -0,0 +1,88 @@
+package model.entities;
+
+import java.util.Objects;
+
+public class User {
+
+	private Integer id;
+	private String name;
+	private String email;
+	private String address;
+	private Boolean isAdmin = false; 
+	
+	public User() {
+	}
+
+	public User(Integer id, String name, String email, String address, Boolean isAdmin) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.isAdmin = isAdmin;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "User [id: " + id + ", name: " + name + ", email: " + email + ", address: " + address + ", isAdmin: "
+				+ isAdmin + "]";
+	}
+	
+	
+}
